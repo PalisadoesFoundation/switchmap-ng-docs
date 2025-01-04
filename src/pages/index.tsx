9faa489bd2e05f94343639766bc6d4a5a7add27c
Abhi <abhiramims333@gmail.com>
2025-01-03 22:16:37 -0500

    Update the site's CSS and NavBar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,9 +18,14 @@ function HomepageHeader() {
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link
+            className="button button--primary button--lg"
+            to="/docs/intro">
+            Introduction
+          </Link>
+          <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            Community
           </Link>
         </div>
       </div>
