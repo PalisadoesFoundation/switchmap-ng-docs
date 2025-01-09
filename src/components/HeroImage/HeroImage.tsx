6d488a1eaf0da958f59c72a10f88ae5d91c63ca0
Abhi <abhiramims333@gmail.com>
2025-01-09 22:00:45 +0530

    Simplify code and fils for better readability

---
+++ src/components/HeroImage/HeroImage.tsx
@@ -0,0 +1,13 @@
+import './HeroImage.css';
+
+export default function HeroImage() {
+  return (
+    <section>
+      <img
+        className="header-image"
+        src="/img/image-02.png"
+        alt="People"
+      />
+    </section>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,47 +1,32 @@
 import clsx from 'clsx';
-import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
-import Heading from '@theme/Heading';
+import HeroImage from '@site/src/components/HeroImage/HeroImage';
 
 import styles from './index.module.css';
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
-        <Heading as="h1" className="hero__title">
-          {siteConfig.title}
-        </Heading>
+        <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--primary button--lg"
-            to="/docs/introduction">
-            Introduction
-          </Link>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/community">
-            Community
-          </Link>
-        </div>
       </div>
     </header>
   );
 }
 
-export default function Home(): JSX.Element {
-  const {siteConfig} = useDocusaurusContext();
+export default function Home() {
+  const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={siteConfig.title}
+      description="Description will go into a meta tag in <head />"
+    >
       <HomepageHeader />
       <main>
-        <HomepageFeatures />
+        <HeroImage />
       </main>
     </Layout>
   );
