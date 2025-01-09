e3cef6b1610eed99a526c3f12afff55f67634c37
Abhi <abhiramims333@gmail.com>
2025-01-09 15:22:35 +0530

    Added markdown files pages for Introduction and Community

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,12 +19,12 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--primary button--lg"
-            to="/docs/intro">
+            to="/docs/introduction">
             Introduction
           </Link>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
+            to="/docs/community">
             Community
           </Link>
         </div>
